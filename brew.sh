258e121dcff688145988f5326b76a0f5a3c28593
Jon Eisen <jon@joneisen.me>
2015-10-16 17:52:21 -0400

    update to my preferences

--- brew.sh
+++ brew.sh
@@ -20,7 +20,7 @@ brew install coreutils
 sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
-brew install moreutils
+# brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
